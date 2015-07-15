79eb04a4c2f17ca4473c275609bdcbcbe47e0414
Lefort, Maxence C <maxence.lefort@nist.gov>
2015-07-15 16:16:53 -0400

    Update validation-build.sh

---
+++ vagrant-ressources/validation-build.sh
@@ -0,0 +1,4 @@
+sudo rm -r /vagrant/project/validation
+git clone https://github.com/hl7-tools/v2-validation-legacy.git /vagrant/project/validation
+cd /vagrant/project/validation
+#TODO figure out how to build (sbt publishLocal ?)
