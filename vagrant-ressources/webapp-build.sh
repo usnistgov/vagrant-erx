6ab260f8b9fa1d12b22969c9d723fe9d6298ee4e
Lefort, Maxence C <maxence.lefort@nist.gov>
2015-08-27 01:13:29 -0400

    Updated build script

--- vagrant-ressources/webapp-build.sh
+++ vagrant-ressources/webapp-build.sh
@@ -1,5 +1,5 @@
 function buildFrontEnd {
-    cd /vagrant/project/webapp/hit-base-tool/hit-base-web/client
+    cd /vagrant/project/webapp/hit-erx-tool/hit-erx-web/client
     npm install
     bower install
     grunt build
@@ -9,27 +9,27 @@ function buildJars {
     cd /vagrant/project/webapp/hit-core
     #git pull
     mvn clean install -DskipTests
-    cd /vagrant/project/webapp/hit-core-hl7v2
+    cd /vagrant/project/webapp/hit-core-erx
     #git pull
     mvn clean install -DskipTests
-    cd /vagrant/project/webapp/hit-resource-bundle
+    cd /vagrant/project/webapp/erx-resource-bundle
     #git pull
     mvn clean install -DskipTests
-    cd /vagrant/project/webapp/hit-base-tool
+    cd /vagrant/project/webapp/hit-erx-tool
     #git pull
     mvn clean install -DskipTests
 }
 
 function deployWar {
-    sudo rm /var/lib/tomcat7/webapps/hit-base-tool.war
-    sudo rm -r /var/lib/tomcat7/webapps/hit-base-tool
-    sudo cp /vagrant/project/webapp/hit-base-tool/hit-base-web/target/hit-base-tool.war /var/lib/tomcat7/webapps/
+    sudo rm /var/lib/tomcat7/webapps/hit-erx-tool.war
+    sudo rm -r /var/lib/tomcat7/webapps/hit-erx-tool
+    sudo cp /vagrant/project/webapp/hit-erx-tool/hit-base-web/target/hit-erx-tool.war /var/lib/tomcat7/webapps/
     cd /var/lib/tomcat7/webapps/
-    sudo chmod +r hit-base-tool.war
+    sudo chmod +r hit-erx-tool.war
     sudo service tomcat7 restart
 }
 
-if [ $# >0 ]; then
+if [ $# == 1 ]; then
     case "$1" in
         '-onlyFrontend')
             buildFrontEnd
